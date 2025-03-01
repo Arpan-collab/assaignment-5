ec23247b6d1a80636e5f8341dbb29e1fe3be8525
Arpan-collab <deyr00081@gmail.com>
2025-03-01 08:00:38 +0600

    folder not found problem solving

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
+// linkup with blogs page
 document.getElementById("goToBlogsPage").addEventListener("click", function () {
-  window.location.href = "/blogs.html";
+  window.location.href = "./blogs.html";
 });
+// linkup blogs to main page 
 function backToHomePageButton() {
-    window.location.href = "/index.html";
+    window.location.href = "./index.html";
 }
