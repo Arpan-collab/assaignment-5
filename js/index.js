dc77ceb5f8ac61fcd7a3986106331a202127eaee
Arpan-collab <deyr00081@gmail.com>
2025-03-01 07:56:00 +0600

    linkup with blogs.html page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-console.log('connected........');
+document.getElementById("goToBlogsPage").addEventListener("click", function () {
+  window.location.href = "/blogs.html";
+});
+function backToHomePageButton() {
+    window.location.href = "/index.html";
+}
