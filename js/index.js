2b460a6d41da080bf565b2d6279333ddd02c2b9f
Arpan-collab <deyr00081@gmail.com>
2025-03-01 08:03:57 +0600

    working on folder not found

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // linkup with blogs page
 document.getElementById("goToBlogsPage").addEventListener("click", function () {
-  window.location.href = "./blogs.html";
+  window.location.href = "/blogs.html";
 });
 // linkup blogs to main page 
 function backToHomePageButton() {
